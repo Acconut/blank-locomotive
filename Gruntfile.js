4a7fba773158447b6ad76d00fabc0f167b69def8
Acconut <maerious@gmail.com>
2013-11-07 20:33:59 +0100

    Added grunt task to compile scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,22 +16,55 @@ module.exports = function(grunt) {
             }
         },
         
+        /**
+         * Combine all scripts into one file
+         */
+        uglify: {
+            dev: {
+                options: {
+                    compress: false,
+                    beautify: true
+                },
+                files: {
+                    "public/scripts/script.js": "app/scripts/views/**/*.js"
+                }
+            },
+            vendor: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    "public/scripts/vendor.js": "app/scripts/vendor/**/*.js"
+                }
+            }
+                
+        },
         
         /**
-         * Recompile all less files when one changes
+         * Recompile all less or js files when one changes
          */
         watch: {
             less: {
                 files: "app/styles/**/*.less",
                 tasks: ["less:dev"]
+            },
+            js: {
+                /**
+                 * We only watch for script in views/ not vendor/
+                 */
+                files: "app/scripts/views/**/*.js",
+                tasks: ["uglify:dev"]
             }
             
         }
     });
     
     grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-contrib-watch");
     
-    grunt.registerTask("css", ["less:dev", "watch:less"]);
+    grunt.registerTask("css", ["less:dev"]);
+    grunt.registerTask("js", ["uglify:dev", "uglify:vendor"]);
+    grunt.registerTask("compile", ["css", "js", "watch"]);
     
 };
